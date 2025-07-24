1d3449c12d570d4319d7c9febb8c5a4b7488b0b0
Decor Drapes <pleatedmosquito@gmail.com>
2025-07-24 12:55:07 +0530

    fire

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -3,13 +3,13 @@ import { getAuth } from 'firebase/auth';
 import { getDatabase } from 'firebase/database';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAD4piXNNzWi3z1riCEl07NHnjg2IHcFhc",
-  authDomain: "decor-drapes.firebaseapp.com",
-  projectId: "decor-drapes",
-  storageBucket: "decor-drapes.firebasestorage.app",
-  messagingSenderId: "175917446618",
-  appId: "1:175917446618:web:1567dfe6a9d9c43e873ce3",
-  measurementId: "G-8W8BZMLVZC",
+  apiKey: "AIzaSyCKi2Irrp1sRKuHFOZDZv27BHsM3Gc3SmE",
+  authDomain: "decor-drapes-instyle.firebaseapp.com",
+  databaseURL: "https://decor-drapes-instyle-default-rtdb.firebaseio.com",
+  projectId: "decor-drapes-instyle",
+  storageBucket: "decor-drapes-instyle.firebasestorage.app",
+  messagingSenderId: "936396093551",
+  appId: "1:936396093551:web:e72e2c2a0aee81fd9e759a"
 };
 const app = initializeApp(firebaseConfig);
 export const db = getDatabase(app);
