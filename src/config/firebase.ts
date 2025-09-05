0062a4d25c93c5195973c4d13dfe301d7c977d1d
Aditya Shah <decordrapes.instyle@gmail.com>
2025-09-05 12:26:50 +0530

    ho ja yaar

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -4,7 +4,7 @@ import { getDatabase } from 'firebase/database';
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  authDomain: "decordrapesinstyle.com",
   databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL,
   projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
   storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
